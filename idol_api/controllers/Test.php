270cfe1ec085edad0a6ae24bd5edae3d6014ac34
Ryan <ryanpulu@outlook.com>
2017-08-08 19:39:27 +0800

    Signed-off-by: Ryan <ryanpulu@outlook.com>primary

---
+++ idol_api/controllers/Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ryan
+ * mail <a>mailto:ryanpulu@outlook.com</a>
+ * Date: 2017/8/8
+ * Time: 18:18
+ */
+class Test extends CI_Controller {
+    public function testfunc(){
+        echo "good";
+    }
+}
